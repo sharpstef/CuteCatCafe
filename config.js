fe2c778b131676e2c16f2e3288f781abf0094259
Stefania Sharp <sharstef@amazon.com>
2021-04-02 09:59:55 -0700

    Adding config template

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    HOST: '',
+    DATABASE: '',
+    USER : '',
+    AUTH: ''
+  });
--- osuConfig.js
+++
@@ -1,6 +0,0 @@
-module.exports = Object.freeze({
-    HOST: 'classmysql.engr.oregonstate.edu',
-    DATABASE: 'cs340_kaczmars',
-    USER : 'cs340_kaczmars',
-    AUTH: '10559176'
-  });
